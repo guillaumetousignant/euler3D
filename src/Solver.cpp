dac14ac272968bac3aa7fad3f687cab0be905ccb
Charles Noel-Laflamme <charles-eric.noel-laflamme@polymtl.ca>
2019-02-13 17:00:37 -0500

    Fonction saveW0

--- src/Solver.cpp
+++ src/Solver.cpp
@@ -46,6 +46,37 @@ void Solver::solve(Block* block)
 void Solver::saveW0(Block* block)
 {
 	cout<<"\tExécution w0: "<<block->test_block_<<endl;
+
+	/*
+	int nb_cells=block->n_cells_in_block_;
+	int cell_idx;
+	double ro, uu, vv, ww, pp;
+	double ro_0,ru_0,rv_0,rw_0,re_0;
+
+	for (cell_idx=0;cell_idx<nb_cells;cell_idx++) //BOUCLER SUR LES CELLULES INTÉRIEURS PLUTÔT? VA DÉPENDRE DE updateSolution
+	{
+		ro=block->primitive_variables_->ro_[cell_idx];
+		uu=block->primitive_variables_->uu_[cell_idx];
+		vv=block->primitive_variables_->vv_[cell_idx];
+		ww=block->primitive_variables_->ww_[cell_idx];
+		pp=block->primitive_variables_->pp_[cell_idx];
+
+		ro_0=ro;
+		ru_0= ro*uu;
+		rv_0= ro*vv;
+		rw_0= ro*ww;
+		re_0= 0.5*ro*(uu*uu+vv*vv)+1./(gamma_-1.0)*pp;
+
+		block->primitive_variables_->ro_0_[cell_idx]=ro_0;
+		block->primitive_variables_->ru_0_[cell_idx]=ru_0;
+		block->primitive_variables_->rv_0_[cell_idx]=rv_0;
+		block->primitive_variables_->rw_0_[cell_idx]=rw_0;
+		block->primitive_variables_->re_0_[cell_idx]=re_0;
+
+
+	}
+
+	*/
 }
 
 
