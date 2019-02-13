dc29b1b6acaafc70fdcd959ba63dcaad61228383
Olivier Jobin <olivier.jobin@polymtl.ca>
2019-02-12 20:03:47 -0500

    Added ways to acces prim. var. in Timestep.cpp

--- src/Timestep.cpp
+++ src/Timestep.cpp
@@ -27,14 +27,27 @@ void Timestep::computeSpectralRadius(Block* block)
 	block->test_block_+=2;
 	cout<<"\t\tExécution computeSpectralRadius: "<<block->test_block_<<endl;
 	// P.211 Blazek !!!	watch out pour delta_s s'il doit multiplier juste c ou non (dépend de comment n définit face_normals_y)
-	// loop on cell
-	//c = sqrt(gamma*pp[cell]/ro[cell]);
-	//		loop on face (on cell)
+	/*
+	my_primitive_variables = block -> block_primitive_variables_;
+	my_ro_array = my_primitive_variables -> ro_;
+	my_uu_array = my_primitive_variables -> uu_;
+	my_vv_array = my_primitive_variables -> vv_;
+	my_ww_array = my_primitive_variables -> ww_;
+	my_pp_array = my_primitive_variables -> pp_;
+
+	gamma = ???
+	*/
+	double c;
+	// for(int cell = 0; cell < ncell; cell++) HOW TO I ACCCES ncell?????
+	//{
+	//c = sqrt(gamma*my_pp_array[cell]/my_ro_array[cell]);
+	//	for(int face = 0; face < face_in_cell; face++) HOW TO I ACCCES face_in_cell?????
+	//{
 	//face =
 	//delta_s = sqrt(norm_x[face]*norm_x[face]+norm_y[face]*norm_y[face]+norm_z[face]*norm_z[face]) !!!!!!!!!!!!!! DELTA_S ATTENTION SCALAIRE/VOLUME
-	//spectral_radius[cell] += fabs(uu[cell]*(*face_normals_x)[face]+vv[cell]*(*face_normals_y)[face]+ww[cell]*(*face_normals_z)[face])+c*delta_s
-	//end loop
-	//end loop
+	//spectral_radius[cell] += fabs(my_uu_array[cell]*(*face_normals_x)[face]+my_vv_array[cell]*(*face_normals_y)[face]+my_ww_array[cell]*(*face_normals_z)[face])+c*delta_s
+	//}
+	//}
 }
 
 void Timestep::computeTimestep(Block* block)
@@ -42,9 +55,10 @@ void Timestep::computeTimestep(Block* block)
 	block->test_block_-=2;
 	cout<<"\t\tExécution computeTimestep: "<<block->test_block_<<endl;
 
-	// loop on CELL
+	// for(int cell = 0; cell < ncell; cell++) HOW TO I ACCCES ncell?????
+	//{
 	//delta_t[cell] = cfl_*cells_areas[cell]/spectral_radius[cell]; VOLUMEEEEEEEEE!
-	//that's it
+	//}
 }
 
 
