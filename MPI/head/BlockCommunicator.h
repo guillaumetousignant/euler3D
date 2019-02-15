99b789fe74994fdcc67d1d41487f11fb3d6c692e
Guillaume Tousignant <moi_guillaume@hotmail.com>
2019-02-15 16:28:59 -0500

    added function prototypes

--- MPI/head/BlockCommunicator.h
+++ MPI/head/BlockCommunicator.h
@@ -1,6 +1,23 @@
+#ifndef BLOCK_COMMUNICATOR_H
+#define BLOCK_COMMUNICATOR_H
+
 class BlockCommunicator {
     public:
-        BlockCommunicator() {
-            
-        }
-}
+        BlockCommunicator() {}
+        ~BlockCommunicator() {}
+
+        int number_of_processes_;
+        int process_id_;
+        int* block_process_id_;
+        int n_blocks_;
+        int n_inter_block_boundaries_;
+        // BoundaryCellIds* inter_block_boundaries_;
+
+        void updateBoundaries();
+        void addCellIdInConnexion();
+        void getMyBlocks(int &n_blocks, int* my_blocks);
+        void initialize();
+        double getGlobal(double coeff_local);
+        void setBoundaryOffset();
+};
+#endif
---
+++ MPI/src/BlockCommunicator.cpp
@@ -0,0 +1,33 @@
+#include "BlockCommunicator.h"
+
+BlockCommunicator::BlockCommunicator(){
+    
+}
+
+BlockCommunicator::~BlockCommunicator(){
+    
+}
+
+void BlockCommunicator::updateBoundaries(){
+
+}
+
+void BlockCommunicator::addCellIdInConnexion(){
+
+}
+
+void BlockCommunicator::getMyBlocks(int &n_blocks, int* my_blocks){
+
+}
+
+void BlockCommunicator::initialize(){
+
+}
+
+double BlockCommunicator::getGlobal(double coeff_local){
+
+}
+
+void BlockCommunicator::setBoundaryOffset(){
+
+}
