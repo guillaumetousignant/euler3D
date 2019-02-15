68b4b7862ce48a0927d22cfc4caafdcdc3f3cf39
frbenb <frederique.benedicto@polymtl.ca>
2019-02-15 16:05:54 -0500

    Placement des titres des sous-sections de la main window 1

--- Python/Interface.py
+++ Python/Interface.py
@@ -1,6 +1,17 @@
 from tkinter import *
-fenetre = Tk()
-label = Label(fenetre, text="Hello World")
-label.pack()
+main_window1 = Tk()
+main_window1.title("1. Input")
 
-fenetre.mainloop()
+title_section1 = LabelFrame(main_window1, text="1.1 Geometry and mesh", padx=20, pady=20)
+title_section1.pack()
+
+text_section1 = Label(title_section1, text="Please select the desired mesh or geometry:", justify=LEFT)
+text_section1.pack()
+
+title_section2 = LabelFrame(main_window1, text="1.2 Input parameters", padx=20, pady=20)
+title_section2.pack()
+
+text_section2 = Label(title_section2, text="Please specify the required input values:", justify=LEFT)
+text_section2.pack()
+
+main_window1.mainloop()
