de5de9eb427c5dd48f625600a61c3a230148d82e
Guillaume Tousignant <moi_guillaume@hotmail.com>
2019-02-15 20:07:58 -0500

    block_process_id

--- MPI/src/BlockCommunicator.cpp
+++ MPI/src/BlockCommunicator.cpp
@@ -6,9 +6,16 @@
 BlockCommunicator::BlockCommunicator(DummyMesh* mesh): n_blocks_(mesh->n_blocks_){
     MPI_Comm_size(MPI_COMM_WORLD, &number_of_processes_);
     MPI_Comm_rank(MPI_COMM_WORLD, &process_id_);
+
     block_process_id_ = new int[n_blocks_];
-    for (int i = 0; i < n_blocks_; i++){
-        //block_process_id_[i] = // how to do this?
+    int n = mesh->n_blocks_/number_of_processes_;
+    int remainder = mesh->n_blocks_%number_of_processes_; 
+
+    // Calculates which process owns what block
+    for (int i = 0; i < number_of_processes_; i++){
+        for (int j = 0; j < n + (i < remainder); j++){
+            block_process_id_[i * n + std::min(i, remainder) + j] = i;
+        }
     }
 }
 
@@ -31,6 +38,10 @@ void BlockCommunicator::getMyBlocks(DummyMesh* mesh) const {
     int remainder = mesh->n_blocks_%number_of_processes_; 
     int index_start = process_id_ * n + std::min(process_id_, remainder);
     mesh->n_my_blocks_ = n + (process_id_ < remainder);
+
+    if (mesh->my_blocks_ != nullptr){
+        delete [] mesh->my_blocks_;
+    }
 
     mesh->my_blocks_ = new DummyBlock*[mesh->n_my_blocks_];
     for (int i = 0; i < mesh->n_my_blocks_; i++){
