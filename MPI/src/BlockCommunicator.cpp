d18ff83abc58986b43abd51816a9fb0bc0b91992
Guillaume Tousignant <moi_guillaume@hotmail.com>
2019-02-15 19:53:25 -0500

    block_process_id

--- MPI/src/BlockCommunicator.cpp
+++ MPI/src/BlockCommunicator.cpp
@@ -7,6 +7,9 @@ BlockCommunicator::BlockCommunicator(DummyMesh* mesh): n_blocks_(mesh->n_blocks_
     MPI_Comm_size(MPI_COMM_WORLD, &number_of_processes_);
     MPI_Comm_rank(MPI_COMM_WORLD, &process_id_);
     block_process_id_ = new int[n_blocks_];
+    for (int i = 0; i < n_blocks_; i++){
+        //block_process_id_[i] = // how to do this?
+    }
 }
 
 BlockCommunicator::~BlockCommunicator(){
@@ -32,9 +35,6 @@ void BlockCommunicator::getMyBlocks(DummyMesh* mesh) const {
     mesh->my_blocks_ = new DummyBlock*[mesh->n_my_blocks_];
     for (int i = 0; i < mesh->n_my_blocks_; i++){
         mesh->my_blocks_[i] = mesh->blocks_[index_start + i];
-        block_process_id_[index_start + i] = process_id_;
-        // REMOVE
-        std::cout << process_id_ << std::endl;
     }    
 }
 
