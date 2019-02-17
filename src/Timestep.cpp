ab13a077b66f622525615d4cafc3e66232affc79
Olivier Jobin <olivier.jobin@polymtl.ca>
2019-02-17 18:07:58 -0500

    work in Timestep.cpp

--- src/Timestep.cpp
+++ src/Timestep.cpp
@@ -31,8 +31,7 @@ void Timestep::computeSpectralRadius(Block* block)
 	/*
 	PrimitiveVariables* my_primitive_variables;
 	my_primitive_variables = block -> block_primitive_variables_;
-
-	double* my_ro_array,my_uu_array,my_vv_array,my_ww_array,my_pp_array
+	double* my_ro_array,my_uu_array,my_vv_array,my_ww_array,my_pp_array;
 	my_ro_array = my_primitive_variables -> ro_;
 	my_uu_array = my_primitive_variables -> uu_;
 	my_vv_array = my_primitive_variables -> vv_;
@@ -43,17 +42,35 @@ void Timestep::computeSpectralRadius(Block* block)
 
 	Cell* my_cells;
 	my_cells = block -> block_cells_;
+	ncell = block -> n_real_cells_in_block_;
+
+	Face* my_faces;
+	my_faces = block -> bloc_faces_;
+	nface = block -> n_faces_in_block_;
+
+	TimeVariables* time_variables;
+	time_variables = block -> block_time_variables_;
+	double** spectral_radius_array;
+	spectral_radius_array = time_variables -> spectral_radius_;
+
+	double* cell_spectral_radius;
+
 	for(int cell_idx = 0; cell_idx < ncell; cell_idx++)
 	{
 		my_cell = my_cells[cell_idx];
 		c = sqrt(gamma_*my_pp_array[my_cell]/my_ro_array[my_cell]);
 
-		TO CONTINUE HERE...
-		for(int face = 0; face < face_in_cell; face++) HOW TO I ACCCES face_in_cell?????
+		// (Je peux faire un sizeof de cell2face, mais on perdrait de la performance, Laurent va pt le faire dans la connectivité, à suivre....)
+		for(int face_idx = 0; face_idx < face_in_cell; face_idx++) HOW TO I ACCCES face_in_cell?????
 		{
-			face =
-			delta_s = sqrt(norm_x[face]*norm_x[face]+norm_y[face]*norm_y[face]+norm_z[face]*norm_z[face]) !!!!!!!!!!!!!! DELTA_S ATTENTION SCALAIRE/VOLUME
-			spectral_radius[cell] += fabs(my_uu_array[cell]*(*face_normals_x)[face]+my_vv_array[cell]*(*face_normals_y)[face]+my_ww_array[cell]*(*face_normals_z)[face])+c*delta_s
+			face_idx =
+
+			cell_spectral_radius = spectral_radius_array[cell_idx];
+
+
+			// .old
+			delta_s = sqrt(norm_x[face_idx]*norm_x[face_idx]+norm_y[face_idx]*norm_y[face_idx]+norm_z[face_idx]*norm_z[face_idx]) !!!!!!!!!!!!!! DELTA_S ATTENTION SCALAIRE/VOLUME
+			spectral_radius[cell_idx] += fabs(my_uu_array[cell_idx]*(*face_normals_x)[face_idx]+my_vv_array[cell_idx]*(*face_normals_y)[face_idx]+my_ww_array[cell_idx]*(*face_normals_z)[face_idx])+c*delta_s
 		}
 	}
 	*/
