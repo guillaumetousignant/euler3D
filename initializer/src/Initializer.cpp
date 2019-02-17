79090f7049c06a9a2b70a4fb215fd6f0ce211fe2
padesp <patrick.deschambault@polymtl.ca>
2019-02-17 09:38:40 -0500

    added class initializer

---
+++ initializer/src/Initializer.cpp
@@ -0,0 +1,17 @@
+#include "Initializer.h"
+
+
+Initializer::Initializer()
+{
+    //Nothing.
+}
+
+Initializer::~Initializer()
+{
+    //Nothing.
+}
+
+void Initializer::doInit()
+{
+    //Nothing.
+}
---
+++ initializer/src/Initializer.h
@@ -0,0 +1,24 @@
+#ifndef DEF_INITIALIZER_H
+#define DEF_INITIALIZER_H
+
+
+class Initializer
+{
+
+    public:
+
+    Initializer();
+    virtual ~Initializer();
+    virtual void doInit() = 0;
+
+
+    protected:
+
+
+    private:
+
+
+};
+
+
+#endif
