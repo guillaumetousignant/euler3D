dc741f7d96d07d089c21650e5c9e0fd50146e49f
Vincent <vincent_liguori@hotmail.com>
2019-02-25 18:25:03 -0500

    mise a jour de interface

--- DataStructure/src/Interface.cpp
+++ DataStructure/src/Interface.cpp
@@ -4,22 +4,22 @@
 #include <iostream>
 using namespace std;
 
-Interface::Interface(string mesh_file_name,string output_file_name, double gamma, double cfl, double angle_of_attack_rad, double angle_of_attack_deg, int stage_number, double mach, double *alpha_rk, double *beta_rk,string interpolation_choice, string gradient_choice, string limiter_choice, string flux_scheme_choice, string residual_smoother_choice, double omega, double k)
+Interface::Interface(string topology_file_name,string output_file_name, double gamma, double cfl, double angle_of_attack_rad, double angle_of_attack_deg, int stage_number, double mach, double cmac, string interpolation_choice, string gradient_choice, string limiter_choice, string flux_scheme_choice, bool residual_smoothing, double omega, double k)
 {
-	mesh_file_name_interface_=mesh_file_name;
+	topology_file_name_interface_=topology_file_name;
 	output_file_name_interface_=output_file_name;
 	gamma_interface_=gamma;
 	cfl_interface_=cfl;
 	angle_of_attack_rad_interface_=angle_of_attack_rad;
 	angle_of_attack_deg_interface_=angle_of_attack_deg;
 	stage_number_interface_=stage_number;
-	alpha_rk_interface_=alpha_rk;
-	beta_rk_interface_=beta_rk;
+	mach_interface_=mach;
+	cmac_interface_=cmac;
 	interpolation_choice_interface_=interpolation_choice;
 	gradient_choice_interface_=gradient_choice;
 	limiter_choice_interface_=limiter_choice;
 	flux_scheme_choice_interface_=flux_scheme_choice;
-	residual_smoother_choice_interface_=residual_smoother_choice;
+	residual_smoothing_interface_=residual_smoothing;
 	omega_interface_=omega;
 	k_interface_=k;
 }
@@ -27,22 +27,20 @@ Interface::Interface(string mesh_file_name,string output_file_name, double gamma
 
 Interface::Interface()
 {	
-	mesh_file_name_interface_="default_mesh.txt";
+	topology_file_name_interface_="default_topology.txt";
 	output_file_name_interface_="default_output.txt";
 	gamma_interface_=1.5;
 	cfl_interface_=1.0;
 	angle_of_attack_rad_interface_=0;
 	angle_of_attack_deg_interface_=0;
 	stage_number_interface_=5;
-	double alpha_rk[5]={420,420,420,420,420};
-	double beta_rk[5]={1337,1337,1337,1337,1337};
-	alpha_rk_interface_=alpha_rk;
-	beta_rk_interface_=beta_rk;
+	mach_interface_=0.5;
+	cmac_interface_=1;
 	interpolation_choice_interface_="Second";
 	gradient_choice_interface_="Least Squares";
 	limiter_choice_interface_="Venkatakrishnan";
 	flux_scheme_choice_interface_="Roe";
-	residual_smoother_choice_interface_="Central IRS";
+	residual_smoothing_interface_=1;
 	omega_interface_=1.0e-14;
 	k_interface_=5;
 }
