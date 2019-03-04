9b9d485063df132a16cb4b74c8dfe16a437ffee4
Guillaume Tousignant <moi_guillaume@hotmail.com>
2019-03-04 15:57:10 -0500

    created after comm object

--- MPI/head/DummyMesh.h
+++ MPI/head/DummyMesh.h
@@ -5,12 +5,12 @@
 
 class DummyMesh {
     public:
-        DummyMesh(int n_blocks);
+        DummyMesh(int n_blocks, int n_blocks_in_process, int* my_blocks);
         ~DummyMesh();
 
         int n_blocks_;
         DummyBlock** blocks_;
-        int n_my_blocks_;
+        int n_blocks_in_process_;
         int* my_blocks_;
 };
 #endif
--- MPI/src/DummyMesh.cpp
+++ MPI/src/DummyMesh.cpp
@@ -1,6 +1,7 @@
 #include "DummyMesh.h"
 
-DummyMesh::DummyMesh(int n_blocks): n_blocks_(n_blocks), n_my_blocks_(0), my_blocks_(nullptr) {
+DummyMesh::DummyMesh(int n_blocks, int n_blocks_in_process, int* my_blocks)
+    : n_blocks_(n_blocks), n_blocks_in_process_(n_blocks_in_process), my_blocks_(my_blocks) {
     blocks_ = new DummyBlock*[n_blocks_];
     for (int i = 0; i < n_blocks_; i++){
         blocks_[i] = new DummyBlock();
