4449dca6a9ebf5aae8bf1b06d15de3abe4a7e6df
Guillaume Tousignant <moi_guillaume@hotmail.com>
2019-03-04 15:57:50 -0500

    now init to nullptr

--- MPI/src/ConnexionCellIds_dummy.cpp
+++ MPI/src/ConnexionCellIds_dummy.cpp
@@ -1,7 +1,7 @@
 #include "ConnexionCellIds_dummy.h"
 
 ConnexionCellIds_dummy::ConnexionCellIds_dummy(int* cell_ids_in_boundary, int n_cell_in_boundary, int* cell_ids_in_boundary_other_block, int block_origin, int block_destination)
-    : BoundaryCellIds_dummy(cell_ids_in_boundary, n_cell_in_boundary), block_origin_(block_origin), block_destination_(block_destination) {
+    : BoundaryCellIds_dummy(cell_ids_in_boundary, n_cell_in_boundary), block_origin_(block_origin), block_destination_(block_destination), cell_ids_in_boundary_other_block_(nullptr) {
     cell_ids_in_boundary_other_block_ = new int[n_cell_in_boundary_];
     for (int i = 0; i < n_cell_in_boundary_; i++){
         cell_ids_in_boundary_other_block_[i] = cell_ids_in_boundary_other_block[i];
