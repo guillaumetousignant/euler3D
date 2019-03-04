92791e05908b4fc7b3a30b451aa8c05f0b21186b
frbenb <frederique.benedicto@polymtl.ca>
2019-03-04 12:55:04 -0500

    Tab 3 commencee et tab 2 faite

--- Python/Interface.py
+++ Python/Interface.py
@@ -28,79 +28,106 @@ class MainWindow():
         title_section_1_1.grid(row=0, column=0, columnspan=10)
 
         text_section_1_1 = ttk.Label(title_section_1_1, text="Please select the desired mesh or geometry: ")
-        text_section_1_1.grid(row=0, column=0, columnspan=10, sticky='W')
+        text_section_1_1.grid(row=0, column=0, columnspan=10, sticky=NSEW)
 
         import_mesh = Button(title_section_1_1, text="Import mesh", command=self.importMesh)
-        import_mesh.grid(row=1, column=1)
+        import_mesh.grid(row=1, column=1, sticky=NSEW)
 
         import_geometry = Button(title_section_1_1, text="Import geometry", command=self.importGeometry)
-        import_geometry.grid(row=1, column=3)
+        import_geometry.grid(row=1, column=3, sticky=NSEW)
 
         generate_geometry = Button(title_section_1_1, text="Generate geometry", command=self.generateGeometry)
-        generate_geometry.grid(row=1, column=5)
+        generate_geometry.grid(row=1, column=5, sticky=NSEW)
 
         show_resulting_mesh = Button(title_section_1_1, text="Show resulting mesh", command=self.showResultingMesh)
-        show_resulting_mesh.grid(row=2, column=3)
+        show_resulting_mesh.grid(row=2, column=3, sticky=NSEW)
 
         # SECTION 1.2 INPUT VALUES
         title_section_1_2 = ttk.LabelFrame(tab1, text="1.2 Input parameters")
         title_section_1_2.grid(row=1, column=0, columnspan=10)
 
         text_section_1_2 = ttk.Label(title_section_1_2, text="Please specify the required input values: ")
-        text_section_1_2.grid(row=0, column=0, columnspan=10, sticky='W')
+        text_section_1_2.grid(row=0, column=0, columnspan=10, sticky=NSEW)
 
-        ttk.Label(title_section_1_2, text="cfl").grid(row=1, column=0)
-        ttk.Entry(title_section_1_2).grid(row=1, column=1)
+        ttk.Label(title_section_1_2, text="cfl", borderwidth=2, relief="groove").grid(row=1, column=0, sticky=NSEW)
+        ttk.Entry(title_section_1_2, width=18).grid(row=1, column=1)
 
-        ttk.Label(title_section_1_2, text="gamma").grid(row=2, column=0)
-        ttk.Entry(title_section_1_2).grid(row=2, column=1)
+        ttk.Label(title_section_1_2, text="gamma", borderwidth=2, relief="groove").grid(row=2, column=0, sticky=NSEW)
+        ttk.Entry(title_section_1_2, width=18).grid(row=2, column=1)
 
-        ttk.Label(title_section_1_2, text="mach").grid(row=3, column=0)
-        ttk.Entry(title_section_1_2).grid(row=3, column=1)
+        ttk.Label(title_section_1_2, text="mach", borderwidth=2, relief="groove").grid(row=3, column=0, sticky=NSEW)
+        ttk.Entry(title_section_1_2, width=18).grid(row=3, column=1)
 
-        ttk.Label(title_section_1_2, text="cmac").grid(row=1, column=2)
-        ttk.Entry(title_section_1_2).grid(row=1, column=3)
+        ttk.Label(title_section_1_2, text="cmac", borderwidth=2, relief="groove").grid(row=1, column=2, sticky=NSEW)
+        ttk.Entry(title_section_1_2, width=18).grid(row=1, column=3)
 
-        ttk.Label(title_section_1_2, text="Angle of attack (deg)").grid(row=2, column=2)
-        ttk.Entry(title_section_1_2).grid(row=2, column=3)
+        ttk.Label(title_section_1_2, text="Angle of\nattack (deg)", borderwidth=2, relief="groove").grid(row=2, column=2, sticky=NSEW)
+        ttk.Entry(title_section_1_2, width=18).grid(row=2, column=3)
 
-        ttk.Label(title_section_1_2, text="Number of stages (RK)").grid(row=3, column=2)
-        ttk.Entry(title_section_1_2).grid(row=3, column=3)
+        ttk.Label(title_section_1_2, text="Number of\nstages (RK)", borderwidth=2, relief="groove").grid(row=3, column=2, sticky=NSEW)
+        ttk.Entry(title_section_1_2, width=18).grid(row=3, column=3)
 
-        # label_nb_block = ttk.Label(title_section_1_1, text="Number of blocks")
-        # label_nb_block.grid(row=4, column=2, columnspan=2)
+        # SECTION 2. SOLVER
+        # SECTION 2.1 CONVERGENCE CRITERIA
+        title_section_2_1 = ttk.LabelFrame(tab2, text="2.1 Convergence criteria")
+        title_section_2_1.grid(row=0, column=0, columnspan=10)
 
-        # nb_block = ttk.Entry(title_section_1_1)
-        # nb_block.grid(row=4, column=4, columnspan=2)
+        text_section_2_1 = ttk.Label(title_section_2_1, text="Please specify the required convergence criteria: ")
+        text_section_2_1.grid(row=0, column=0, columnspan=10, sticky=NSEW)
 
-        # nb_process = Frame(text="Number of process")
-        # nb_process.grid()
+        ttk.Label(title_section_2_1, text="Max number\nof itterations", borderwidth=2, relief="groove").grid(row=1, column=1, sticky=NSEW)
+        ttk.Entry(title_section_2_1, width=18).grid(row=1, column=2)
 
+        ttk.Label(title_section_2_1, text="Convergence\ncriterion", borderwidth=2, relief="groove").grid(row=2, column=1, sticky=NSEW)
+        ttk.Entry(title_section_2_1, width=18).grid(row=2, column=2)
+
+        # SECTION 2.2 SCHEME SELECTION
+        title_section_2_2 = ttk.LabelFrame(tab2, text="2.2 Scheme Selection")
+        title_section_2_2.grid(row=1, column=0, columnspan=10)
+
+        text_section_2_2 = ttk.Label(title_section_2_2, text="Please select the required flux scheme: ")
+        text_section_2_2.grid(row=0, column=0, columnspan=10, sticky=NSEW)
+
+        flux_scheme = ttk.Button(title_section_2_2, text="Select\nflux scheme", command=self.selectFluxScheme)
+        flux_scheme.grid(row=1, column=1)
+
+        # SECTION 2.3 SOLVER OPTIONS
+        title_section_2_3 = ttk.LabelFrame(tab2, text="2.3 Solver options")
+        title_section_2_3.grid(row=2, column=0, columnspan=10)
+
+        text_section_2_3 = ttk.Label(title_section_2_3, text="Please select a code option: ")
+        text_section_2_3.grid(row=0, column=0, columnspan=10, sticky=NSEW)
+
+        # SECTION 3 OUTPUT
+        text_section_3 = ttk.Label(tab3, text="Please specify what type of graphe(s) you would like to display: ")
+        text_section_3.grid(row=0, column=0)
+
+    # SECTION 1 METHODS
     def importMesh(self):
-        self.meshwindow = Tk()
-        self.meshwindow.title("Mesh importation")
-        ttk.Label(self.meshwindow, text="Import the desired mesh: ").grid(row=0, column=0)
-        ttk.Button(self.meshwindow, text="Select a file", command=self.meshFileDialog).grid(row=1, column=2)
-        ttk.Button(self.meshwindow, text="Ok", command=self.meshwindow.destroy).grid(row=2, column=1)
-        ttk.Button(self.meshwindow, text="Cancel", command=self.meshwindow.destroy).grid(row=2, column=2)
+        self.mesh_window = Tk()
+        self.mesh_window.title("Mesh importation")
+        ttk.Label(self.mesh_window, text="Import the desired mesh: ").grid(row=0, column=0)
+        ttk.Button(self.mesh_window, text="Select a file", command=self.meshFileDialog).grid(row=1, column=2)
+        ttk.Button(self.mesh_window, text="Ok", command=self.mesh_window.destroy).grid(row=2, column=1)
+        ttk.Button(self.mesh_window, text="Cancel", command=self.mesh_window.destroy).grid(row=2, column=2)
 
     def meshFileDialog(self):
         self.filename = filedialog.askopenfilename(initialdir="/", title="Select a file", filetypes=(("jpeg", "*.jpg"),("All Files", "*.*")))
-        self.label = ttk.Label(self.meshwindow, text="")
+        self.label = ttk.Label(self.mesh_window, text="")
         self.label.grid(row=1, column=1)
         self.label.configure(text=self.filename)
 
     def importGeometry(self):
-        self.geometrywindow = Tk()
-        self.geometrywindow.title("Geometry importation")
-        ttk.Label(self.geometrywindow, text="Import the desired geometry: ").grid(row=0, column=0)
-        ttk.Button(self.geometrywindow, text="Select a file", command=self.geometryFileDialog).grid(row=1, column=2)
-        ttk.Button(self.geometrywindow, text="Ok", command=self.geometrywindow.destroy).grid(row=2, column=1)
-        ttk.Button(self.geometrywindow, text="Cancel", command=self.geometrywindow.destroy).grid(row=2, column=2)
+        self.geometry_window = Tk()
+        self.geometry_window.title("Geometry importation")
+        ttk.Label(self.geometry_window, text="Import the desired geometry: ").grid(row=0, column=0)
+        ttk.Button(self.geometry_window, text="Select a file", command=self.geometryFileDialog).grid(row=1, column=2)
+        ttk.Button(self.geometry_window, text="Ok", command=self.geometry_window.destroy).grid(row=2, column=1)
+        ttk.Button(self.geometry_window, text="Cancel", command=self.geometry_window.destroy).grid(row=2, column=2)
 
     def geometryFileDialog(self):
         self.filename = filedialog.askopenfilename(initialdir="/", title="Select a file", filetypes=(("jpeg", "*.jpg"),("All Files", "*.*")))
-        self.label = ttk.Label(self.geometrywindow, text="")
+        self.label = ttk.Label(self.geometry_window, text="")
         self.label.grid(row=1, column=1)
         self.label.configure(text=self.filename)
 
@@ -109,49 +136,15 @@ class MainWindow():
 
     def showResultingMesh(self):
         messagebox.showinfo('Resulting Mesh')
-        
     
+    # SECTION 2 METHODS
+    def selectFluxScheme(self):
+        self.flux_scheme_window = Tk()
+        self.flux_scheme_window.title("Scheme selection")
+        ttk.Label(self.flux_scheme_window, text="Please select the desired scheme: ").grid(row=0, column=0)
+        ttk.Button(self.flux_scheme_window, text="Ok", command=self.flux_scheme_window.destroy).grid(row=2, column=1)
+        ttk.Button(self.flux_scheme_window, text="Cancel", command=self.flux_scheme_window.destroy).grid(row=2, column=2)
 
-        # SECTION 1.2 INPUT PARAMETERS
-
-
-
-
-# # SECTION 1.2: INPUT PARAMETERS
-# title_section_1_2 = LabelFrame(tab1, text="1.2 Input parameters", padx=20, pady=20)
-# title_section_1_2.pack(fill="both", expand="yes")
-
-# text_section_1_2 = Label(title_section_1_2, text="Please specify the required input values:")
-# text_section_1_2.pack()
-
-# ##################### 2. SOLVER #####################
-# # SECTION 2.1: CONVERGENCE CRITERIAS
-# title_section_2_1 = LabelFrame(tab2, text="2.1 Convergence criterias", padx=20, pady=20)
-# title_section_2_1.pack(fill="both", expand="yes")
-
-# text_section_2_1 = Label(title_section_2_1, text="Please specify the required convergence criterias:")
-# text_section_2_1.pack()
-
-# # SECTION 2.2: SCHEME SELECTION
-# title_section_2_2 = LabelFrame(tab2, text="2.2 Scheme Selection", padx=20, pady=20)
-# title_section_2_2.pack(fill="both", expand="yes")
-
-# text_section_2_2 = Label(title_section_2_2, text="Please select the desired flux scheme:")
-# text_section_2_2.pack()
-
-# # SECTION 2.3: SOLVER OPTIONS
-# title_section_2_3 = LabelFrame(tab2, text="2.3 Solver Options", padx=20, pady=20)
-# title_section_2_3.pack(fill="both", expand="yes")
-
-# text_section_2_3 = Label(title_section_2_3, text="Please select other code options:")
-# text_section_2_3.pack()
-
-# ##################### 3. OUTPUT #####################
-# text_section_3 = Label(tab3, text="Please specify the graphs that you would like to display:")
-# text_section_3.pack()
-
-# DISPLAYING OF THE MAIN WINDOW
-# main_window1.mainloop()
 
 root = Tk()
 main_window = MainWindow(root)
