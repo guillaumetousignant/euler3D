06c9e6b00f858c7837df9fc4922d25134c0179fc
laurentpb <laurentpb@outlook.com>
2019-03-12 15:40:46 -0400

    antihoraire

--- BlockBuilder/src/BlockBuilder.cpp
+++ BlockBuilder/src/BlockBuilder.cpp
@@ -11,6 +11,7 @@
 #include "Block.h"
 #include <iostream>
 #include <string>
+#include <math.h>
 
 BlockBuilder::BlockBuilder(std::string block_file)
 {
@@ -144,9 +145,9 @@ Face* BlockBuilder::buildFace(int face_id, int n_nodes_per_face,FaceCreator* fac
 
 void BlockBuilder::setConnectivity(Block* block)
 {
-	int i,j,k,idx,node_0,node_1,node_2,n_cells_linked;
+	int i,j,k,w,idx,node_0,node_1,node_2,n_cells_linked;
 	int *idx_node,*idx_cell_2_faces,*idx_cell_2_cells; 
-	double THETA[10],THETA_TEMP,centre_TEMP,VECTX[10],VECTY[10],VECTZ[10],norm1,norm2,cos_TEMP;
+	double THETA[10],THETA_TEMP,centre_TEMP,VECTX[10],VECTY[10],VECTZ[10],norm1,norm2,cos_TEMP,PROJ_1[3],PROJ_2[3],face_center[3];
 	int face2nodes_TEMP;
 
 	
@@ -273,7 +274,7 @@ std::cout<<"DÉBUT cell2facesconnectivity"<<std::endl;
 			idx_cell_2_cells[block->block_faces_[i]->face_2_cells_connectivity_[1]]+=1;
 
 		}
-	
+
 
 	//VÉRIFICATION
 	/*	
@@ -285,52 +286,112 @@ std::cout<<"DÉBUT cell2facesconnectivity"<<std::endl;
 	}
 	*/
 
-		/*
+		
 std::cout<<"DÉBUT CENTRE CELLS"<<std::endl;
 
 std::cout<<"DÉBUT face2nodes CCW"<<std::endl;
 
-for(i=0;;i <= block->n_faces_in_block_-1 ;i++)
+for(i=0;i <= block->n_faces_in_block_-1 ;i++)
 {
+	face_center[0]=0;
+	face_center[1]=0;
+	face_center[2]=0;
+	/*
 	block->block_faces_[i]->face_center_[0]=0;
 	block->block_faces_[i]->face_center_[1]=0;
 	block->block_faces_[i]->face_center_[2]=0;
+	*/
 	for(j=0;j <= block->block_faces_[i]->n_nodes_per_face_-1;j++)
 	{
-		block->block_faces_[i]->face_center_[0]+=block->block_nodes_[block->block_faces_[i]->face_2_nodes_connectivity_[j]]->node_coordinates[0];
-		block->block_faces_[i]->face_center_[1]+=block->block_nodes_[block->block_faces_[i]->face_2_nodes_connectivity_[j]]->node_coordinates[0];
-		block->block_faces_[i]->face_center_[2]+=block->block_nodes_[block->block_faces_[i]->face_2_nodes_connectivity_[j]]->node_coordinates[0];
+
+		face_center[0]+=block->block_nodes_[block->block_faces_[i]->face_2_nodes_connectivity_[j]]->node_coordinates_[0];
+		face_center[1]+=block->block_nodes_[block->block_faces_[i]->face_2_nodes_connectivity_[j]]->node_coordinates_[0];
+		face_center[2]+=block->block_nodes_[block->block_faces_[i]->face_2_nodes_connectivity_[j]]->node_coordinates_[0];
+		/*
+		block->block_faces_[i]->face_center_[0]+=block->block_nodes_[block->block_faces_[i]->face_2_nodes_connectivity_[j]]->node_coordinates_[0];
+		block->block_faces_[i]->face_center_[1]+=block->block_nodes_[block->block_faces_[i]->face_2_nodes_connectivity_[j]]->node_coordinates_[0];
+		block->block_faces_[i]->face_center_[2]+=block->block_nodes_[block->block_faces_[i]->face_2_nodes_connectivity_[j]]->node_coordinates_[0];
+		*/
 	}
+	face_center[0]=face_center[0]/block->block_faces_[i]->n_nodes_per_face_;
+	face_center[1]=face_center[1]/block->block_faces_[i]->n_nodes_per_face_;
+	face_center[2]=face_center[2]/block->block_faces_[i]->n_nodes_per_face_;
+
+	VECTX[0]=block->block_nodes_[block->block_faces_[i]->face_2_nodes_connectivity_[0]]->node_coordinates_[0]-face_center[0];
+	VECTY[0]=block->block_nodes_[block->block_faces_[i]->face_2_nodes_connectivity_[0]]->node_coordinates_[1]-face_center[1];
+	VECTZ[0]=block->block_nodes_[block->block_faces_[i]->face_2_nodes_connectivity_[0]]->node_coordinates_[2]-face_center[2];	
+	/*
 	block->block_faces_[i]->face_center_[0]=block->block_faces_[i]->face_center_[0]/block->block_faces_[i]->n_nodes_per_face_;
 	block->block_faces_[i]->face_center_[1]=block->block_faces_[i]->face_center_[1]/block->block_faces_[i]->n_nodes_per_face_;
 	block->block_faces_[i]->face_center_[2]=block->block_faces_[i]->face_center_[2]/block->block_faces_[i]->n_nodes_per_face_;
 
-	VECTX[0]=block->block_nodes_[block->block_faces_[i]->face_2_nodes_connectivity_[0]]->node_coordinates[0]-block->block_faces_[i]->face_center_[0];
-	VECTY[0]=block->block_nodes_[block->block_faces_[i]->face_2_nodes_connectivity_[0]]->node_coordinates[1]-block->block_faces_[i]->face_center_[1];
-	VECTZ[0]=block->block_nodes_[block->block_faces_[i]->face_2_nodes_connectivity_[0]]->node_coordinates[2]-block->block_faces_[i]->face_center_[2];
-
+	VECTX[0]=block->block_nodes_[block->block_faces_[i]->face_2_nodes_connectivity_[0]]->node_coordinates_[0]-block->block_faces_[i]->face_center_[0];
+	VECTY[0]=block->block_nodes_[block->block_faces_[i]->face_2_nodes_connectivity_[0]]->node_coordinates_[1]-block->block_faces_[i]->face_center_[1];
+	VECTZ[0]=block->block_nodes_[block->block_faces_[i]->face_2_nodes_connectivity_[0]]->node_coordinates_[2]-block->block_faces_[i]->face_center_[2];
+	*/
 	norm1=sqrt(VECTX[0]*VECTX[0]+VECTY[0]*VECTY[0]+VECTZ[0]*VECTZ[0]);
+
+	THETA[0]=0;
 	
 	for(j=1;j <= block->block_faces_[i]->n_nodes_per_face_-1;j++)
 	{
-
-		VECTX[j]=block->block_nodes_[block->block_faces_[i]->face_2_nodes_connectivity_[j]]->node_coordinates[0]-block->block_faces_[i]->face_center_[0];
-		VECTY[j]=block->block_nodes_[block->block_faces_[i]->face_2_nodes_connectivity_[j]]->node_coordinates[1]-block->block_faces_[i]->face_center_[1];
-		VECTZ[j]=block->block_nodes_[block->block_faces_[i]->face_2_nodes_connectivity_[j]]->node_coordinates[2]-block->block_faces_[i]->face_center_[2];
+		VECTX[j]=block->block_nodes_[block->block_faces_[i]->face_2_nodes_connectivity_[j]]->node_coordinates_[0]-face_center[0];
+		VECTY[j]=block->block_nodes_[block->block_faces_[i]->face_2_nodes_connectivity_[j]]->node_coordinates_[1]-face_center[1];
+		VECTZ[j]=block->block_nodes_[block->block_faces_[i]->face_2_nodes_connectivity_[j]]->node_coordinates_[2]-face_center[2];
+		/*
+		VECTX[j]=block->block_nodes_[block->block_faces_[i]->face_2_nodes_connectivity_[j]]->node_coordinates_[0]-block->block_faces_[i]->face_center_[0];
+		VECTY[j]=block->block_nodes_[block->block_faces_[i]->face_2_nodes_connectivity_[j]]->node_coordinates_[1]-block->block_faces_[i]->face_center_[1];
+		VECTZ[j]=block->block_nodes_[block->block_faces_[i]->face_2_nodes_connectivity_[j]]->node_coordinates_[2]-block->block_faces_[i]->face_center_[2];
+		*/
 
 		norm2=sqrt(VECTX[j]*VECTX[j]+VECTY[j]*VECTY[j]+VECTZ[j]*VECTZ[j]);
 
 
 		cos_TEMP=(VECTX[0]*VECTX[j]+VECTY[0]*VECTY[j]+VECTZ[0]*VECTZ[j])/(norm1*norm2);
 
+		PROJ_1[0]=((norm2*cos_TEMP)/norm1)*VECTX[0];
+		PROJ_1[1]=((norm2*cos_TEMP)/norm1)*VECTY[1];
+		PROJ_1[2]=((norm2*cos_TEMP)/norm1)*VECTZ[2];
+
+		PROJ_2[0]=VECTX[j]-PROJ_1[0];
+		PROJ_2[1]=VECTY[j]-PROJ_1[1];
+		PROJ_2[2]=VECTZ[j]-PROJ_1[2];
+
+		THETA[j]=atan2(sqrt(PROJ_2[0]*PROJ_2[0]+PROJ_2[1]*PROJ_2[1]+PROJ_2[2]*PROJ_2[2]),sqrt(PROJ_1[0]*PROJ_1[0]+PROJ_1[1]*PROJ_1[1]+PROJ_1[2]*PROJ_1[2]));
+
+		face2nodes_TEMP=block->block_faces_[i]->face_2_nodes_connectivity_[j];
+		THETA_TEMP=THETA[j];
+		for(k=0;k<j;k++)
+		{
+			if(THETA[j]<THETA[k])
+			{
+				for(w=j;w>k;w--)
+				{
+					block->block_faces_[i]->face_2_nodes_connectivity_[w]=block->block_faces_[i]->face_2_nodes_connectivity_[w-1];
+					THETA[w]=THETA[w-1];
+				}
+				block->block_faces_[i]->face_2_nodes_connectivity_[k]=face2nodes_TEMP;
+				k=j;
+			}
+		}
 
 	}
-
 
 }
 
 
-*/
+	//VÉRIFICATION
+	
+	for(i=0;i <= block->n_faces_in_block_-1 ;i++)
+	{
+		std::cout << i << " "<< block->block_faces_[i]->face_2_nodes_connectivity_[0] << " "<< block->block_faces_[i]->face_2_nodes_connectivity_[1] << " "<< block->block_faces_[i]->face_2_nodes_connectivity_[2] << " "<< block->block_faces_[i]->face_2_nodes_connectivity_[3]<<std::endl;
+		//std::cout << i <<" Nb faces "<< block->block_cells_[i]->n_faces_per_cell_<<" CELL2CELLS "<< block->block_cells_[i]->cell_2_cells_connectivity_[0] <<" " << block->block_cells_[i]->cell_2_cells_connectivity_[1] <<" " << block->block_cells_[i]->cell_2_cells_connectivity_[2] <<" " << block->block_cells_[i]->cell_2_cells_connectivity_[3] <<" " << block->block_cells_[i]->cell_2_cells_connectivity_[4] <<" " << block->block_cells_[i]->cell_2_faces_connectivity_[5]  <<std::endl;
+
+	}
+	
+
+
+
 
 }
 
