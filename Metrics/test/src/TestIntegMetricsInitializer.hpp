816824d2b3e26845fc6f934257d856ff210f7d40
padesp <patrick.deschambault@polymtl.ca>
2019-03-13 10:36:41 -0400

    remove ../ before loading filename

--- Metrics/test/src/TestIntegMetricsInitializer.hpp
+++ Metrics/test/src/TestIntegMetricsInitializer.hpp
@@ -19,7 +19,7 @@ using namespace std;
 void buildConnectivityInteg(Block *new_block)
 {
     
-    string block_file ="../UnstructuredMesh5x5.su2";
+    string block_file ="UnstructuredMesh5x5.su2";
 
     ConcreteBlockBuilder concrete_block_builder = ConcreteBlockBuilder(block_file);
     concrete_block_builder.preReadMyBlock(new_block);
