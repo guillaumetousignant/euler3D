59d0b428db02961c95104e8fcf75fc43a90eccf3
frbenb <frederique.benedicto@polymtl.ca>
2019-03-13 18:41:16 -0400

    Ajout de valeurs par defaut dans la tab output

--- Python/output.py
+++ Python/output.py
@@ -18,60 +18,75 @@ class Output():
         text_section_3.grid(row=0, column=0, columnspan=3, sticky=NSEW)
 
         self.cl_alpha = IntVar()
+        self.cl_alpha.set(0)
         Checkbutton(title_section_3, text="Cl vs alpha", variable=self.cl_alpha, relief="groove", width=20, anchor=W, borderwidth=2).grid(row=1, column=0, padx=2, pady=2)
             
         self.cd_alpha = IntVar()
+        self.cd_alpha.set(0)
         Checkbutton(title_section_3, text="Cd vs alpha", variable=self.cd_alpha, relief="groove", width=20, anchor=W, borderwidth=2).grid(row=2, column=0, padx=2, pady=2)
 
         self.cm_alpha = IntVar()
+        self.cm_alpha.set(0)
         Checkbutton(title_section_3, text="Cm vs alpha", variable=self.cm_alpha, relief="groove", width=20, anchor=W, borderwidth=2).grid(row=3, column=0, padx=2, pady=2)
 
         self.cl_convergence = IntVar()
+        self.cl_convergence.set(0)
         Checkbutton(title_section_3, text="Cl convergence", variable=self.cl_convergence, relief="groove", width=20, anchor=W, borderwidth=2).grid(row=4, column=0, padx=2, pady=2)
 
         self.cd_convergence = IntVar()
+        self.cd_convergence.set(0)
         Checkbutton(title_section_3, text="Cd convergence", variable=self.cd_convergence, relief="groove", width=20, anchor=W, borderwidth=2).grid(row=5, column=0, padx=2, pady=2)
 
         self.cm_convergence = IntVar()
+        self.cm_convergence.set(0)
         Checkbutton(title_section_3, text="Cm convergence", variable=self.cm_convergence, relief="groove", width=20, anchor=W, borderwidth=2).grid(row=6, column=0, padx=2, pady=2)
 
         self.residual_convergence = IntVar()
+        self.residual_convergence.set(0)
         Checkbutton(title_section_3, text="Residual convergence", variable=self.residual_convergence, relief="groove", width=20, anchor=W, borderwidth=2).grid(row=7, column=0, padx=2, pady=2)
 
         self.cp_xc = IntVar()
+        self.cp_xc.set(0)
         Checkbutton(title_section_3, text="Cp vs Xc", variable=self.cp_xc, relief="groove", width=20, anchor=W, borderwidth=2).grid(row=1, column=1, columnspan=2, padx=2, pady=2)
 
         self.mach_contour = IntVar()
+        self.mach_contour.set(0)
         Checkbutton(title_section_3, text="Mach contour", variable=self.mach_contour, relief="groove", width=20, anchor=W, borderwidth=2).grid(row=2, column=1, columnspan=2, padx=2, pady=2)
 
         self.cp_contour = IntVar()
+        self.cp_contour.set(0)
         Checkbutton(title_section_3, text="Cp contour", variable=self.cp_contour, relief="groove", width=20, anchor=W, borderwidth=2).grid(row=3, column=1, columnspan=2, padx=2, pady=2)
 
         self.slice_cp = IntVar()
+        self.slice_cp.set(0)
         Checkbutton(title_section_3, text="Slice Cp", variable=self.slice_cp, command=self.activateAxisOfCut, relief="groove", width=20, anchor=W, borderwidth=2).grid(row=4, column=1, columnspan=2, padx=2, pady=2)
 
-        self.axis_cut_var = StringVar()
+        self.axis_cut = StringVar()
+        self.axis_cut.set("x")
         self.axis_cut_label = ttk.Label(title_section_3, text="Axis of cut", relief="groove", state="disabled", borderwidth=2, width=10, anchor=CENTER)
         self.axis_cut_label.grid(row=5, column=1)
-        self.axis_cut_entry = ttk.Entry(title_section_3, textvariable=self.axis_cut_var, width=11, state="disabled")
+        self.axis_cut_entry = ttk.Entry(title_section_3, textvariable=self.axis_cut, width=11, state="disabled")
         self.axis_cut_entry.grid(row=5, column=2, padx=2, pady=2)
 
-        self.x_coord_var = DoubleVar()    
+        self.x_coord = DoubleVar()    
+        self.x_coord.set(1.0)
         self.x_coord_label = ttk.Label(title_section_3, text="X coord", relief="groove", state="disabled", borderwidth=2, width=10, anchor=CENTER)
         self.x_coord_label.grid(row=6, column=1)
-        self.x_coord_entry = ttk.Entry(title_section_3, textvariable=self.x_coord_var, width=11, state="disabled")
+        self.x_coord_entry = ttk.Entry(title_section_3, textvariable=self.x_coord, width=11, state="disabled")
         self.x_coord_entry.grid(row=6, column=2, padx=2, pady=2)
 
-        self.y_coord_var = DoubleVar()    
+        self.y_coord = DoubleVar()   
+        self.y_coord.set(1.0) 
         self.y_coord_label = ttk.Label(title_section_3, text="Y coord", relief="groove", state="disabled", borderwidth=2, width=10, anchor=CENTER)
         self.y_coord_label.grid(row=7, column=1)
-        self.y_coord_entry = ttk.Entry(title_section_3, textvariable=self.y_coord_var, width=11, state="disabled")
+        self.y_coord_entry = ttk.Entry(title_section_3, textvariable=self.y_coord, width=11, state="disabled")
         self.y_coord_entry.grid(row=7, column=2, padx=2, pady=2)
 
-        self.z_coord_var = DoubleVar()
+        self.z_coord = DoubleVar()
+        self.z_coord.set(1.0)
         self.z_coord_label = ttk.Label(title_section_3, text="Z coord", relief="groove", state="disabled", borderwidth=2, width=10, anchor=CENTER)
         self.z_coord_label.grid(row=8, column=1)
-        self.z_coord_entry = ttk.Entry(title_section_3, textvariable=self.z_coord_var, width=11, state="disabled")
+        self.z_coord_entry = ttk.Entry(title_section_3, textvariable=self.z_coord, width=11, state="disabled")
         self.z_coord_entry.grid(row=8, column=2, padx=2, pady=2)
 
         # GENERAL BUTTONS
@@ -116,10 +131,10 @@ class Output():
         mach_cont_str = str(self.mach_contour.get())
         cp_cont_str = str(self.cp_contour.get())
         slice_cp_str = str(self.slice_cp.get())
-        axis_str = self.axis_cut_var.get()
-        xcoord_str = str(self.x_coord_var.get())
-        ycoord_str = str(self.y_coord_var.get())
-        zcoord_str = str(self.z_coord_var.get())
+        axis_str = self.axis_cut.get()
+        xcoord_str = str(self.x_coord.get())
+        ycoord_str = str(self.y_coord.get())
+        zcoord_str = str(self.z_coord.get())
 
         partial_output = "\nOUTPUT (0-no 1-yes)\nclalpha cdalpha cmalpha\n"+(
                         cl_alpha_str+" "+cd_alpha_str+" "+cm_alpha_str)+(
