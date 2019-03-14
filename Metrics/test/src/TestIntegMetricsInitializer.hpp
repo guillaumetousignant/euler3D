b338f6439d7fdbe942abc646a109ab0ae33cb5dc
padesp <patrick.deschambault@polymtl.ca>
2019-03-14 13:08:15 -0400

    adding test for normales integration

--- Metrics/test/src/TestIntegMetricsInitializer.hpp
+++ Metrics/test/src/TestIntegMetricsInitializer.hpp
@@ -14,7 +14,7 @@
 
 using namespace std;
 
-
+typedef unsigned int uint;
 
 void buildConnectivityInteg(Block *new_block)
 {
@@ -61,8 +61,50 @@ TEST_CASE("Test computation of center cell (ID = 0)", "")
 }
 
 TEST_CASE("Compute Normales (cellID = 0)")
-{
+{   
+    //Making sure to respect orientation left to right for cells
 
+
+    Block *blockData = new Block(0);
+    buildConnectivityInteg(blockData);
+
+    uint cellRefID = 0;
+    uint cellNeighborID;
+
+    std::vector<double> centerCellNeighbor(3);
+
+    std::vector<double> centerCellRef;
+    centerCellRef.push_back(blockData->block_cells_[cellRefID]->cell_coordinates_[0]);
+    centerCellRef.push_back(blockData->block_cells_[cellRefID]->cell_coordinates_[1]);
+    centerCellRef.push_back(blockData->block_cells_[cellRefID]->cell_coordinates_[2]);
+
+    std::vector<double> connectCenterVec(3);
+
+    uint nbCells = blockData->n_real_cells_in_block_;
+
+
+    for(uint i(0);i < nbCells;i++)
+    {
+        uint nbFacesCell = blockData->block_cells_[i]->n_faces_per_cell_;
+
+        for(uint j(0);j < nbFacesCell;j++)
+        {
+            uint faceID = blockData->block_cells_[i]->cell_2_faces_connectivity_[j];
+
+            uint cellLeftID = blockData->block_faces_[faceID]->face_2_cells_connectivity_[0];
+            uint cellRightID = blockData->block_faces_[faceID]->face_2_cells_connectivity_[1];
+
+            
+
+        }
+
+
+    }
+
+    
+    
+    delete blockData;
+    blockData = nullptr;
 
 
 }
