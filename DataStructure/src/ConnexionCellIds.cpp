dac1ff556da448cab05f838421a89367c0988ec4
Guillaume Tousignant <moi_guillaume@hotmail.com>
2019-04-08 12:49:50 -0400

    sets blocks to -1

--- DataStructure/src/ConnexionCellIds.cpp
+++ DataStructure/src/ConnexionCellIds.cpp
@@ -11,7 +11,7 @@ void ConnexionCellIds::updateBoundary()
 	std::cout<<"Connexion cells updated" << std::endl;
 }
 
-ConnexionCellIds::ConnexionCellIds() : cell_ids_in_boundary_other_block_(nullptr)
+ConnexionCellIds::ConnexionCellIds() : cell_ids_in_boundary_other_block_(nullptr), block_origin_(-1), block_destination_(-1)
 {
 
 }
